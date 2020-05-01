a458b7af86db64d87020086048491d3aa5d4ea22
Ugur Kaya <49519312+ugurcankaya@users.noreply.github.com>
2020-05-01 18:00:47 +0300

    Update array_ex0.c

--- array_ex0.c
+++ array_ex0.c
@@ -2,15 +2,15 @@
 
 
 /*
- Write a C program which reads 5 integers from the user and put the
+ Write a C program which reads 10 integers from the user and put the
  integers between 10 and 50 (both included) into an array and print it.
  */
 
 
 int main(void){
-    int a[10], n;
+    int a[10], n, z = 0;
     
-    for (int i = 1, z = 0; i <= 10; i++) {
+    for (int i = 1; i <= 10; i++) {
         printf("Enter an integer: ");
         scanf("%d", &n);
         if (n>=10 && n<=50) {
@@ -20,12 +20,11 @@ int main(void){
     
     }
     
-    for (n = 0; n<10; n++) {
+    for (n = 0; n<z; n++) {
         printf("a[%d] = %d\n", n, a[n]);
         
     }
     
-    printf("size of a is %lu", sizeof(a)/sizeof(a[0]));
     
     
 }
